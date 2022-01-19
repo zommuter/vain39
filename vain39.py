df6a1863e36b2bf5f718ff521f46915a73f17b54
Tobias Kienzler <zommuter@users.noreply.github.com>
2022-01-19 14:25:29 +0100

    refactor

--- vain39.py
+++ vain39.py
@@ -6,7 +6,6 @@ from bip_utils import (
     Bip39WordsNum, Bip39MnemonicGenerator, Bip39SeedGenerator, Bip44Changes, Bip44Coins, Bip44, Bip84Coins, Bip84, Bip49Coins, Bip49
 )
 
-
 #%% Generate random mnemonic
 def new_mnemonic():
     return Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_24)
@@ -14,60 +13,46 @@ def new_mnemonic():
 mnemonic = new_mnemonic()
 print(f"Mnemonic string: {mnemonic}")
 
-#%% Generate seed from mnemonic
-seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
-# Construct from seed
-bip44_mst_ctx = Bip44.FromSeed(seed_bytes, Bip44Coins.BITCOIN)
-# Print master key
-print(f"Master key (bytes): {bip44_mst_ctx.PrivateKey().Raw().ToHex()}")
-print(f"Master key (extended): {bip44_mst_ctx.PrivateKey().ToExtended()}")
-print(f"Master key (WIF): {bip44_mst_ctx.PrivateKey().ToWif()}")
+class Bipper(object):
+    def __init__(self, bip=44, seed_bytes:bytes = None, mnemonic=None):
+        self.Bip = {44: Bip44, 49: Bip49, 84: Bip84}[bip]
+        self.coin_type = {44: Bip44Coins.BITCOIN, 49: Bip49Coins.BITCOIN, 84: Bip84Coins.BITCOIN}[bip]
+        assert (seed_bytes is None) or (mnemonic is None), "Cannot provide both seed_bytes and mnemonic"
+        if seed_bytes is None:
+            # Generate seed from mnemonic
+            self.mnemonic = mnemonic or new_mnemonic()
+            self.seed_bytes = Bip39SeedGenerator(self.mnemonic).Generate()
+        else:  # seed_bytes provided, hence mnemonic is None
+            self.mnemonic = None
+            self.seed_bytes = seed_bytes
 
-# Construct from seed
-bip84_mst_ctx = Bip84.FromSeed(seed_bytes, Bip84Coins.BITCOIN)
-# Print master key
-print(f"Master key (bytes): {bip84_mst_ctx.PrivateKey().Raw().ToHex()}")
-print(f"Master key (extended): {bip84_mst_ctx.PrivateKey().ToExtended()}")
-print(f"Master key (WIF): {bip84_mst_ctx.PrivateKey().ToWif()}")
+        # Construct from seed
+        self.mst_ctx = self.Bip.FromSeed(self.seed_bytes, self.coin_type)
 
-# Construct from seed
-bip49_mst_ctx = Bip49.FromSeed(seed_bytes, Bip49Coins.LITECOIN)
-# Print master key
-print(f"Master key (bytes): {bip49_mst_ctx.PrivateKey().Raw().ToHex()}")
-print(f"Master key (extended): {bip49_mst_ctx.PrivateKey().ToExtended()}")
-print(f"Master key (WIF): {bip49_mst_ctx.PrivateKey().ToWif()}")
+        # Print master key
+        #print(f"Master key (bytes): {self.mst_ctx.PrivateKey().Raw().ToHex()}")
+        #print(f"Master key (extended): {self.mst_ctx.PrivateKey().ToExtended()}")
+        #print(f"Master key (WIF): {self.mst_ctx.PrivateKey().ToWif()}")
 
-#%% Generate BIP44 account keys: m/44'/0'/0'
-bip44_acc_ctx = bip44_mst_ctx.Purpose().Coin().Account(0)
-# Generate BIP44 chain keys: m/44'/0'/0'/0
-bip44_chg_ctx = bip44_acc_ctx.Change(Bip44Changes.CHAIN_EXT)
+    def generate_account_keys(self, account:int = 0):
+        self.acc_ctx = self.mst_ctx.Purpose().Coin().Account(account)
+        self.chg_ctx = self.acc_ctx.Change(Bip44Changes.CHAIN_EXT)
 
-# Generate BIP84 account keys: m/49'/0'/0'
-bip84_acc_ctx = bip84_mst_ctx.Purpose().Coin().Account(0)
-# Generate BIP49 chain keys: m/49'/0'/0'/0
-bip84_chg_ctx = bip84_acc_ctx.Change(Bip44Changes.CHAIN_EXT)
+    def get_address(self, account:int = None, address_index:int = 0):
+        if account is not None: self.generate_account_keys(account=account)
+        return self.chg_ctx.AddressIndex(address_index)
 
-# Generate BIP49 account keys: m/49'/0'/0'
-bip49_acc_ctx = bip49_mst_ctx.Purpose().Coin().Account(0)
-# Generate BIP49 chain keys: m/49'/0'/0'/0
-bip49_chg_ctx = bip49_acc_ctx.Change(Bip44Changes.CHAIN_EXT)
+bippers = {}
+for bip in (44, 84, 49):
+    print(f"BIP{bip}")
+    bippers[bip] = Bipper(bip=bip)
+    bippers[bip].generate_account_keys()
 
-
-#%% Generate the first 10 addresses: m/44'/0'/0'/0/i
-for i in range(10):
-    bip44_addr_ctx = bip44_chg_ctx.AddressIndex(i)
-    print(f"{i}. Address public key (extended): {bip44_addr_ctx.PublicKey().ToExtended()}")
-    print(f"{i}. Address private key (extended): {bip44_addr_ctx.PrivateKey().ToExtended()}")
-    print(f"{i}. Address: {bip44_addr_ctx.PublicKey().ToAddress()}")
-
-    bip84_addr_ctx = bip84_chg_ctx.AddressIndex(i)
-    print(f"{i}. Address public key (extended): {bip84_addr_ctx.PublicKey().ToExtended()}")
-    print(f"{i}. Address private key (extended): {bip84_addr_ctx.PrivateKey().ToExtended()}")
-    print(f"{i}. Address: {bip84_addr_ctx.PublicKey().ToAddress()}")
-
-    bip49_addr_ctx = bip49_chg_ctx.AddressIndex(i)
-    print(f"{i}. Address public key (extended): {bip49_addr_ctx.PublicKey().ToExtended()}")
-    print(f"{i}. Address private key (extended): {bip49_addr_ctx.PrivateKey().ToExtended()}")
-    print(f"{i}. Address: {bip49_addr_ctx.PublicKey().ToAddress()}")
+# Generate the first 3 addresses: m/44'/0'/0'/0/i
+    for i in range(3):
+        addr_ctx = bippers[bip].get_address(i)
+        #print(f"{i}. Address public key (extended): {addr_ctx.PublicKey().ToExtended()}")
+        #print(f"{i}. Address private key (extended): {addr_ctx.PrivateKey().ToExtended()}")
+        print(f"{i}. Address: {addr_ctx.PublicKey().ToAddress()}")
 
 # %%
