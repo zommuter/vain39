ebabc50b672b18de7436a9822c6a41fae188336d
Tobias Kienzler <zommuter@users.noreply.github.com>
2022-01-19 20:44:47 +0100

    autocreate addresses

--- vain39.py
+++ vain39.py
@@ -8,7 +8,10 @@ def new_mnemonic():
     return Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_24)
 
 class Bipper(object):
-    def __init__(self, bip=44, seed_bytes:bytes = None, mnemonic=None, coin_type = "BITCOIN"):
+    def __init__(self, bip=44,
+                 seed_bytes:bytes = None, mnemonic=None,
+                 coin_type = "BITCOIN",
+                 accounts = 1, addresses = 20):
         self.Bip: Bip44Base = {44: Bip44, 49: Bip49, 84: Bip84}[bip]
         self.coin_type = {44: Bip44Coins, 49: Bip49Coins, 84: Bip84Coins}[bip][coin_type]
         assert (seed_bytes is None) or (mnemonic is None), "Cannot provide both seed_bytes and mnemonic"
@@ -23,24 +26,17 @@ class Bipper(object):
         # Construct from seed
         self.mst_ctx = self.Bip.FromSeed(self.seed_bytes, self.coin_type)
 
-        self.accounts = {}
-
-    def get_account_key(self, account:int = 0):
-        if not account in self.accounts:
-            acc_ctx = self.mst_ctx.Purpose().Coin().Account(account)
-            self.accounts[account] = {
-                'acc_ctx': acc_ctx,
-                'chg_ctx': acc_ctx.Change(Bip44Changes.CHAIN_EXT),
-                'address_index': 0
-            }
-        return self.accounts[account]
+        self.acc_ctxs = [self.mst_ctx.Purpose().Coin().Account(i) for i in range(accounts)]
+        self.chg_ctxs = [acc_ctx.Change(Bip44Changes.CHAIN_EXT) for acc_ctx in self.acc_ctxs]
+        self.address_indices = [0] * accounts
+        self.keys = [[self.get_address(address_index=address_index, account=account)[0] for address_index in range(addresses)] for account in range(accounts)]
+        self.addresses = [[self.keys[account][address_index].PublicKey().ToAddress() for address_index in range(addresses)] for account in range(accounts)]
 
     def get_address(self, address_index:int = None, account:int = 0):
-        account_key = self.get_account_key(account=account)
-        chg_ctx: Bip44Base = account_key['chg_ctx']
+        chg_ctx: Bip44Base = self.chg_ctxs[account]
         if address_index is None:
-            address_index = account_key['address_index']
-            account_key['address_index'] += 1
+            address_index = self.address_indices[account]
+            self.address_indices[account] += 1
         return (chg_ctx.AddressIndex(address_index), address_index)
 
 #%% Generate random mnemonic
@@ -53,6 +49,7 @@ for bip in (44, 84, 49, -44):
     coin_type = "ETHEREUM" if bip == -44 else "BITCOIN"
     print(f"BIP{bip}")
     bippers[bip] = Bipper(bip=abs(bip), coin_type=coin_type, mnemonic=mnemonic)
+    print(bippers[bip].addresses)
 
 #%% Generate 3 addresses: m/44'/0'/0'/0/i
 for bip in (44, 84, 49, -44):
